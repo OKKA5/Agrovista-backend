ed4bbd24128d2272f6595b19239f90f722687da4
OKKA5 <anasossama123@gmail.com>
2026-07-04 02:22:42 +0300

    ipv4 installation

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import { Injectable } from "@nestjs/common";
+import * as nodemailer from "nodemailer";
 @Injectable()
 export class MailService {
   private transporter;
 
   constructor() {
-
     this.transporter = nodemailer.createTransport({
-      pool: true,
-      service: 'gmail', 
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS?.replace(/\s/g, ''),
+        pass: process.env.EMAIL_PASS?.replace(/\s/g, ""),
       },
-    });
+    } as any);
   }
 
   async sendVerificationCode(email: string, code: string, message: string) {
@@ -25,8 +26,8 @@ export class MailService {
         text: `Your verification code is: ${code}`,
       });
     } catch (error) {
-      console.error('Error sending email:', error);
-      throw new Error('Could not send verification email.');
+      console.error("Error sending email:", error);
+      throw new Error("Could not send verification email.");
     }
   }
 }
