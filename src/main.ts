4b4e5a837a568c2bef06ac7fc7649039f444914c
OKKA5 <anasossama123@gmail.com>
2026-07-04 11:06:45 +0300

    CORS solving

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,43 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './filters/http-exception.filter';
-import cookieParser from 'cookie-parser';
-
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./filters/http-exception.filter";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
-        const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-        app.use(cookieParser());
+  app.use(cookieParser());
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  });
 
-        app.useGlobalPipes(
-                new ValidationPipe({
-                        whitelist: true,
-                        forbidNonWhitelisted: true,
-                        transform: true,
-                }),
-        );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
-        app.useGlobalFilters(
-                new HttpExceptionFilter()
-        );
+  app.useGlobalFilters(new HttpExceptionFilter());
 
-        app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix("api/v1");
 
-        const config = new DocumentBuilder()
-                .setTitle('Agrovista API')
-                .setDescription('Agrovista Agricultural Platform API')
-                .setVersion('1.0')
-                .addBearerAuth()
-                .build();
-        const document = SwaggerModule.createDocument(app, config);
-        SwaggerModule.setup('api/docs', app, document);
+  const config = new DocumentBuilder()
+    .setTitle("Agrovista API")
+    .setDescription("Agrovista Agricultural Platform API")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document);
 
-        const port = process.env.PORT || 3000;
-        await app.listen(port);
-        console.log(`Application running on port ${port}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application running on port ${port}`);
 }
 bootstrap();
