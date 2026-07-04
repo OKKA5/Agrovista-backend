aa5e11b8133a808896837647ffcc16525a62a3b6
OKKA5 <anasossama123@gmail.com>
2026-07-04 03:26:33 +0300

    switch from sendgrid to mailgun

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,27 +1,30 @@
 import { Injectable, Logger } from "@nestjs/common";
-import sgMail from "@sendgrid/mail";
+import Mailgun from "mailgun.js";
+import formData from "form-data";
 
 @Injectable()
 export class MailService {
   private readonly logger = new Logger(MailService.name);
+  private mg;
 
   constructor() {
-    if (!process.env.SENDGRID_API_KEY) {
-      this.logger.error("SENDGRID_API_KEY is not set");
-    }
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+    const mailgun = new Mailgun(formData);
+    this.mg = mailgun.client({
+      username: "api",
+      key: process.env.MAILGUN_API_KEY ?? "",
+    });
   }
 
   async sendVerificationCode(email: string, code: string, message: string) {
     try {
-      await sgMail.send({
-        to: email,
+      await this.mg.messages.create(process.env.MAILGUN_DOMAIN!, {
         from: process.env.EMAIL_USER!,
+        to: [email],
         subject: `Verification Code ${message}`,
         text: `Your verification code is: ${code}`,
       });
-    } catch (error: any) {
-      this.logger.error("SendGrid error:", error?.response?.body || error);
+    } catch (error) {
+      this.logger.error("Mailgun error:", error?.response?.body || error);
       throw new Error("Could not send verification email.");
     }
   }
